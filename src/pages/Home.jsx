80a784abba51c1588dbd56ad7b235b25b5257adc
Ugo SOREL <u.sorel@axione.group>
2026-07-28 15:41:24 +0200

    Fix typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -263,7 +263,8 @@ export function Home() {
                 className="appearance-none
                   flex items-center gap-1.5
                   px-3 py-1.5
-                  rounded-fulltext-xs font-mono
+                  rounded-full
+                  text-xs font-mono
                   border
                   border-white/10
                   bg-white/5
